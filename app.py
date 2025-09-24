9f7abba57167aa6385f17ef8848cc77e6c27ed7a
Olga Chirkova <lely4e@gmail.com>
2025-09-24 19:06:21 +0200

    Port number changed

--- app.py
+++ app.py
@@ -144,4 +144,4 @@ if __name__ == "__main__":
     # # Create tables
     # with app.app_context():
     #     db.create_all()
-    app.run(host="127.0.0.1", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5002, debug=True)
